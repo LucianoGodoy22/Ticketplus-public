bab17d4f97b1ddca0643bd4b413e43afb8f07977
LucianoGodoy22 <luci.godoy@duocuc.cl>
2026-09-12 19:25:57 -0300

    Deploy Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Ticketplus-public/', // DEBE estar entre barras (ej: /ticketplus-publico/)
+  server: {
+    port: 5174 // Puerto local distinto para correr ambos a la vez
+  }
 })
